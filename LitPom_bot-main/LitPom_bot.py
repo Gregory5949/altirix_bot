5c92db7d0b213aea520d13dca175c2ed77e894af
Grigory D <grigorydro@gmail.com>
2024-12-15 22:02:48 +0300

    Added limit's check for user's queries

--- LitPom_bot-main/LitPom_bot.py
+++ LitPom_bot-main/LitPom_bot.py
@@ -1,3 +1,6 @@
+import datetime
+
+import psycopg2
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.retrievers import MultiQueryRetriever, ContextualCompressionRetriever, EnsembleRetriever
 from langchain_community.chat_models import GigaChat
@@ -12,24 +15,27 @@ from langchain_community.retrievers import BM25Retriever
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_retrieval_chain
-
-from langchain_community.document_loaders import UnstructuredWordDocumentLoader
-from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_community.document_loaders import UnstructuredXMLLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.runnables import RunnableWithMessageHistory
 
-from config import sber, bot_token
-from langgraph.graph import START, MessagesState, StateGraph
+from config import sber, bot_token, connection_params
 
 user_conversations = {}  # Словарь для хранения ConversationBufferMemory каждого пользователя
 user_llm_rag = {}  # Словарь для хранения модели и rag каждого пользователя
 
 doc_store = 'data'  # Путь для сохранения векторных хранилищ
 
-qa_system_prompt = '''Ты чат-бот помощник, созданный компанией Альтирикс системс. You MUST answer to questions only in Russian. Вы отвечаете только на вопросы по темам информационной безопасности, компьютерной безопасности и кибербезопасности. Если вопрос задан на другом языке или в вашем хранилище RAG нет информации для ответа, сообщите об этом пользователю. Не отвечайте на общие вопросы, такие как "Как дела?" и вопросы, заданные не на русском языке. Ответ должен быть до 300 символов. Прежде чем ответить, проанализируйте запрос и определите его тематическую область.
+qa_system_prompt = '''Ты чат-бот помощник, созданный компанией Альтирикс системс. You MUST answer to questions only in Russian. Вы отвечаете только на вопросы по темам информационной безопасности, компьютерной безопасности и кибербезопасности. Если вопрос задан на другом языке или в вашем хранилище RAG нет информации для ответа, сообщите об этом пользователю. Не отвечайте на общие вопросы, такие как "Как дела?" и вопросы, заданные не на русском языке. Если вопрос связан с нормативными документами, необходимо привести фрагмент из опредленного нормативного документа, обосновывающего ответ. Ответ должен подтвержаться Ответ должен быть до 200 символов. Прежде чем ответить, проанализируйте запрос и определите его тематическую область.
 Контекст: {context}
 Вопрос: {input}'''
+
+# qa_system_prompt = '''Действуйте как эксперт в области информационной безопасноти и права. Ответьте на вопрос на основе приведенного ниже контекста, и если на вопрос нельзя ответить на основе контекста, скажите "Я не знаю".
+# Контекст: {context}
+# Вопрос: {input}'''
+
+# qa_system_prompt = 'Ты — чат-бот помощник, созданный компанией Альтирикс Системс. Ты отвечаешь на вопросы только на русском языке и ограничиваешься темами информационной безопасности, компьютерной безопасности и кибербезопасности. Если вопрос задан на другом языке или в твоем хранилище RAG нет информации для ответа, сообщи об этом пользователю. Не отвечай на общие вопросы (например, "Как дела?"). Для вопросов, связанных с нормативными документами, обязательно приводи фрагмент из соответствующего документа, обосновывающего ответ. Перед ответом проанализируй запрос и уточни его тематическую область. Если запрос неясен, предложи пользователю уточнить вопрос.' \
+# 'Context: {context}' \
+# 'Question: {input}'
+
 
 qa_prompt = ChatPromptTemplate.from_messages(
     [
@@ -38,7 +44,6 @@ qa_prompt = ChatPromptTemplate.from_messages(
         ("human", "{input}"),
     ]
 )
-# Создать объект бота
 import telebot
 from time import sleep
 from telebot import types
@@ -58,10 +63,42 @@ contextualize_q_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 from langchain_community.chat_message_histories import ChatMessageHistory
+
+import psycopg2
+from datetime import datetime, timedelta
+
+
+def check_rate_limit(user_id, user_message):
+    with psycopg2.connect(**connection_params) as connection:
+        with connection.cursor() as cursor:
+
+    # connection = psycopg2.connect("dbname=<your_db> user=<your_user> password=<your_password>")
+    # cursor = connection.cursor()
+
+    # Check how many requests were made in the last 24 hours
+            cursor.execute("""
+                SELECT COUNT(*) FROM request_logs 
+                WHERE user_id = %s AND created_at >= NOW() - INTERVAL '1 day';
+            """, (user_id,))
+
+            count = cursor.fetchone()[0]
+
+            if count >= 10:
+                return False  # Limit reached
+            else:
+                # Log the new request
+                cursor.execute("INSERT INTO request_logs (user_id, user_message) VALUES (%s, %s);", (user_id, user_message))
+                connection.commit()
+                return True  # Request allowed
+
+
+
+
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
     if session_id not in user_conversations:
         user_conversations[session_id] = ChatMessageHistory()
     return user_conversations[session_id]
+
 
 def create_llm_rag(user_id):
     llm = GigaChat(credentials=sber,
@@ -72,11 +109,22 @@ def create_llm_rag(user_id):
 
     embeddings = GigaChatEmbeddings(credentials=sber, verify_ssl_certs=False)
     vector_store = Chroma(
-        persist_directory="/Users/gd/Desktop/altirix_bot_chromadbs/chroma_db_docs28885_ch_size500_ch_overlap50_gigachat_embs",
+                   persist_directory="/Users/gd/PycharmProjects/altirix_systems_chatbot/chromadb_chunk_size_1200",
         embedding_function=embeddings)
+        # persist_directory="/Users/gd/Desktop/altirix_bot_chromadbs/chroma_db_docs28885_ch_size500_ch_overlap50_gigachat_embs",
+        # embedding_function=embeddings)
 
-    print(len(vector_store._collection.get(include=['embeddings'])['embeddings']))
-    print(vector_store._collection_name)
+    # print(len(vector_store._collection.get(include=['embeddings'])['embeddings']))
+    # print(vector_store._collection_name)
+    retriever_vanilla = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 8, })
+    retriever_mmr = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 8,})
+
+
+    retriever_BM25 = BM25Retriever.from_texts(vector_store.get()['documents'])
+
+    ensemble_retriever = EnsembleRetriever(
+        retrievers=[retriever_vanilla, retriever_mmr, retriever_BM25], weights=[0.3, 0.3, 0.4]
+    )
 
     # embedding_retriever = MultiQueryRetriever.from_llm(
     #     retriever=vector_store.as_retriever(
@@ -84,39 +132,15 @@ def create_llm_rag(user_id):
     # ), llm=llm
     # )
 
-    # embedding_retriever = vector_store.as_retriever(
-    #     search_type="mmr",  # Also test "similarity"
-    #     search_kwargs={"k": 8},
-    # )
-
     # compressor = FlashrankRerank()
     # compression_retriever = ContextualCompressionRetriever(
     #     base_compressor=compressor, base_retriever=embedding_retriever
     # )
-
-    # You need to define vectordb first
-    retriever_vanilla = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
-    retriever_mmr = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 4})
-
-    retriever_BM25 = BM25Retriever.from_texts(vector_store.get()['documents'])
-
-    question = "Tell me the roles and responsibilities of the GO cancellation process in Italy."
-
-    # Retrieve documents
-    # vanilla_relevant_docs = retriever_vanilla.invoke(question)
-    # mmr_relevant_docs = retriever_mmr.invoke(question)
-    # bm25_relevant_docs = retriever_BM25.invoke(question)
-
-    # initialize the ensemble retriever with 3 Retrievers
-    ensemble_retriever = EnsembleRetriever(
-        retrievers=[retriever_vanilla, retriever_mmr, retriever_BM25], weights=[0.3, 0.3, 0.4]
-    )
-
-    # ensemble_relevant_docs = ensemble_retriever.invoke(question)
-    # print("Ensemble Relevant Docs to process:", len(ensemble_relevant_docs))
     history_aware_retriever = create_history_aware_retriever(
         llm, ensemble_retriever, contextualize_q_prompt
     )
+
+
     rag_chain = create_rag_chain(user_id, llm, history_aware_retriever)
 
     conversation_chain = create_conversation_chain(user_id, llm)
@@ -124,16 +148,10 @@ def create_llm_rag(user_id):
 
 
 def create_rag_chain(user_id, llm, embedding_retriever):
-    # (1)
-    # Создадим цепочку create_stuff_documents_chain, которая является частью
-    # вопросно-ответной системы, ответственной за вставку фрагментов текстов
-    # из векторной БД в промпт языковой модели
     document_chain = create_stuff_documents_chain(
         llm=llm,
         prompt=qa_prompt
     )
-    # Создадим вопросно-ответную цепочку с помощью функции create_retrieval_chain().
-    # используем ретривет для векторной базы с книгой
     retrieval_chain = create_retrieval_chain(embedding_retriever, document_chain)
 
     conversational_rag_chain = RunnableWithMessageHistory(
@@ -146,44 +164,12 @@ def create_rag_chain(user_id, llm, embedding_retriever):
     return (conversational_rag_chain)
 
 
-# Создать цепочку диалога
-####################################################
 def create_conversation_chain(user_id, llm):
-    # (2)
-    # Создать объект цепочки диалога - инициализация ConversationChain
     conversation = ConversationChain(llm=llm,
                                      verbose=True,
                                      memory=ConversationBufferMemory())
 
-    # Обращение к системе
-    # conversation.predict(input='Как меня зовут и чем я занимаюсь?')
     return (conversation)
-
-
-####################################################
-# Загрузка и векторизация текста
-def learn_document(doc_file, vector_store):
-    # ЗАГРУЗКА И НАРЕЗКА ТЕКСТА DOCX
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024,
-                                                   chunk_overlap=128)
-    # Определить расшиение файла - docx, pdf или fb2
-    file_ext = doc_file[doc_file.rfind(".") + 1:]
-    # Использовать соответствующий загрузчик
-    if file_ext == 'docx':
-        loader = UnstructuredWordDocumentLoader(doc_file)
-    elif file_ext == 'pdf':
-        loader = UnstructuredPDFLoader(doc_file)
-    elif file_ext == 'fb2':
-        loader = UnstructuredXMLLoader(doc_file)
-    else:
-        return (0)
-
-    splitted_data = loader.load_and_split(text_splitter)
-    # ВЕКТОРИЗАЦИЯ
-    # Добавить документ в векторное хранилище
-    ids_list = vector_store.add_documents(splitted_data)
-
-    return (len(ids_list))
 
 
 ####################################################################################################
@@ -196,7 +182,6 @@ def learn_document(doc_file, vector_store):
 def start(message: types.Message):
     user_id = message.chat.id
 
-    # Проверка словарей для данного пользователя
     if user_id not in user_conversations:
         user_conversations[user_id] = ChatMessageHistory()
 
@@ -216,7 +201,7 @@ def help(message: types.Message):
     user_id = message.chat.id
 
     bot.send_message(message.chat.id,
-                     'Я - бот-помощник в работе с текстами. Вы можете отправить мне файлы в форматах .doc .pdf .fb2, я их обработаю, загружу в векторную базу данных и Вы сможете задавать мне вопросы по этим текставм.')
+                     'Я - бот-помощник, отвечающий на вопросы по теме информационной безопасности.')
 
 
 ####################################################################################################
@@ -234,45 +219,7 @@ def help(message: types.Message):
                                     'contact'])
 def not_text(message):
     user_id = message.chat.id
-    bot.send_message(user_id, 'Я работаю только с текстовыми сообщениями и документами!')
-
-
-# Функция, обрабатывающая файлы документов
-##########################################
-@bot.message_handler(content_types=['document'])
-def handle_doc_message(message):
-    user_id = message.chat.id
-
-    # Проверка словарей для данного пользователя
-    if user_id not in user_conversations:
-        user_conversations[user_id] = ConversationBufferMemory()
-
-    if user_id not in user_llm_rag:
-        user_llm_rag[user_id] = create_llm_rag(user_id)
-
-    vdb, embedding_retriever, llm, rag_chain, conversation = user_llm_rag[user_id]
-    conversation.memory = user_conversations[user_id]
-
-    # Загрузка файла
-    file_info = bot.get_file(message.document.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    file_name = message.document.file_name
-    bot.reply_to(message, "Читаю документ: " + file_name)
-    # src = 'C:/Python/Project/tg_bot/files/received/' + file_name;
-    with open(file_name, 'wb') as new_file:
-        new_file.write(downloaded_file)
-
-    # Векторизация документа и добавление в векторное хранилище пользователя
-    r = learn_document(file_name, vdb)
-    # Сообщение пользователю о результате операции
-    if r > 0:
-        bot.send_message(user_id, "Документ прочитан (" + str(r) + ')')
-        # Сохранить дополненное векторное хранилище
-        vdb.save_local(doc_store, str(user_id))
-    else:
-        bot.send_message(user_id, "Не могу прочитать документ, вероятно не понятный формат.")
-
-    sleep(2)
+    bot.send_message(user_id, 'Я работаю только с текстовыми сообщениями!')
 
 
 # Функция, обрабатывающая текстовые сообщения
@@ -281,7 +228,6 @@ def handle_doc_message(message):
 def handle_text_message(message):
     user_id = message.chat.id
 
-    # Проверка словарей для данного пользователя
     if user_id not in user_conversations:
         user_conversations[user_id] = ChatMessageHistory()
 
@@ -291,24 +237,33 @@ def handle_text_message(message):
     vdb, embedding_retriever, llm, rag_chain, conversation = user_llm_rag[user_id]
     conversation.memory = user_conversations[user_id]
 
+    # print(user_conversations[user_id])
     q1 = message.text
-    print(q1)
 
-    # (RAG)
-    resp1 = rag_chain.invoke(
-        {'input': q1}, config={'configurable': {'session_id': user_id}}
-    )
+    for i in vdb.search(q1, search_type='similarity'):
+        print(i.page_content)
 
-    # bot.send_message(user_id, 'RAG (' + q1 + '):')
-    answer = resp1['answer']
-    bot.send_message(user_id, answer)
-    # (LLM)
-    # q2 = answer
-    # resp2 = conversation.predict(input=q2)
-    # bot.send_message(user_id, 'LLM:')
-    # bot.send_message(user_id, conversation.memory.chat_memory.messages[-1].content)
 
-    # ........
+    if check_rate_limit(user_id, q1):
+    # with psycopg2.connect(**connection_params) as connection:
+    #     with connection.cursor() as cursor:
+    #         insert_query = """INSERT INTO user_queries (id_user, user_message, time_sent) VALUES (%s, %s, %s)"""
+    #
+    #         record_to_insert = (user_id, q1, datetime.datetime.now())
+    #
+    #         cursor.execute(insert_query, record_to_insert)
+    #         connection.commit()
+
+
+        resp1 = rag_chain.invoke(
+            {'input': q1}, config={'configurable': {'session_id': user_id}}
+        )
+
+        answer = resp1['answer']
+        bot.send_message(user_id, answer)
+    else:
+        bot.send_message(user_id, "Вы уже отправили боту 10 запросов за эти сутки. " )
+
 
     sleep(2)
 
