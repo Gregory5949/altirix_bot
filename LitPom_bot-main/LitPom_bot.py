284182e3a15e67d729a675b1042e2b155c2375e5
Grigory D <grigorydro@gmail.com>
2024-12-16 11:09:52 +0300

    Clean code

--- LitPom_bot-main/LitPom_bot.py
+++ LitPom_bot-main/LitPom_bot.py
@@ -1,15 +1,11 @@
-import datetime
-
-import psycopg2
+from gigachat.exceptions import ResponseError
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.retrievers import MultiQueryRetriever, ContextualCompressionRetriever, EnsembleRetriever
 from langchain_community.chat_models import GigaChat
-
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_chroma import Chroma
-from langchain_community.document_compressors import FlashrankRerank
 from langchain_community.embeddings import GigaChatEmbeddings
 from langchain_community.retrievers import BM25Retriever
 from langchain_core.chat_history import BaseChatMessageHistory
@@ -19,23 +15,12 @@ from langchain_core.runnables import RunnableWithMessageHistory
 
 from config import sber, bot_token, connection_params
 
-user_conversations = {}  # Словарь для хранения ConversationBufferMemory каждого пользователя
-user_llm_rag = {}  # Словарь для хранения модели и rag каждого пользователя
+user_conversations = {}
+user_llm_rag = {}
 
-doc_store = 'data'  # Путь для сохранения векторных хранилищ
-
-qa_system_prompt = '''Ты чат-бот помощник, созданный компанией Альтирикс системс. You MUST answer to questions only in Russian. Вы отвечаете только на вопросы по темам информационной безопасности, компьютерной безопасности и кибербезопасности. Если вопрос задан на другом языке или в вашем хранилище RAG нет информации для ответа, сообщите об этом пользователю. Не отвечайте на общие вопросы, такие как "Как дела?" и вопросы, заданные не на русском языке. Если вопрос связан с нормативными документами, необходимо привести фрагмент из опредленного нормативного документа, обосновывающего ответ. Ответ должен подтвержаться Ответ должен быть до 200 символов. Прежде чем ответить, проанализируйте запрос и определите его тематическую область.
+qa_system_prompt = '''Ты чат-бот помощник, созданный компанией Альтирикс системс. You MUST answer to questions only in Russian. Вы отвечаете только на вопросы по темам информационной безопасности, компьютерной безопасности и кибербезопасности. Если вопрос задан на другом языке или в вашем хранилище RAG нет информации для ответа, сообщите об этом пользователю. Не отвечайте на общие вопросы, такие как "Как дела?" и вопросы, заданные не на русском языке. Если вопрос связан с нормативными документами, необходимо привести фрагмент из опредленного нормативного документа, обосновывающего ответ. Ответ должен быть до 200 символов. Прежде чем ответить, проанализируйте запрос и определите его тематическую область.
 Контекст: {context}
 Вопрос: {input}'''
-
-# qa_system_prompt = '''Действуйте как эксперт в области информационной безопасноти и права. Ответьте на вопрос на основе приведенного ниже контекста, и если на вопрос нельзя ответить на основе контекста, скажите "Я не знаю".
-# Контекст: {context}
-# Вопрос: {input}'''
-
-# qa_system_prompt = 'Ты — чат-бот помощник, созданный компанией Альтирикс Системс. Ты отвечаешь на вопросы только на русском языке и ограничиваешься темами информационной безопасности, компьютерной безопасности и кибербезопасности. Если вопрос задан на другом языке или в твоем хранилище RAG нет информации для ответа, сообщи об этом пользователю. Не отвечай на общие вопросы (например, "Как дела?"). Для вопросов, связанных с нормативными документами, обязательно приводи фрагмент из соответствующего документа, обосновывающего ответ. Перед ответом проанализируй запрос и уточни его тематическую область. Если запрос неясен, предложи пользователю уточнить вопрос.' \
-# 'Context: {context}' \
-# 'Question: {input}'
-
 
 qa_prompt = ChatPromptTemplate.from_messages(
     [
@@ -71,11 +56,6 @@ from datetime import datetime, timedelta
 def check_rate_limit(user_id, user_message):
     with psycopg2.connect(**connection_params) as connection:
         with connection.cursor() as cursor:
-
-    # connection = psycopg2.connect("dbname=<your_db> user=<your_user> password=<your_password>")
-    # cursor = connection.cursor()
-
-    # Check how many requests were made in the last 24 hours
             cursor.execute("""
                 SELECT COUNT(*) FROM request_logs 
                 WHERE user_id = %s AND created_at >= NOW() - INTERVAL '1 day';
@@ -84,14 +64,12 @@ def check_rate_limit(user_id, user_message):
             count = cursor.fetchone()[0]
 
             if count >= 10:
-                return False  # Limit reached
+                return False
             else:
-                # Log the new request
-                cursor.execute("INSERT INTO request_logs (user_id, user_message) VALUES (%s, %s);", (user_id, user_message))
+                cursor.execute("INSERT INTO request_logs (user_id, user_message) VALUES (%s, %s);",
+                               (user_id, user_message))
                 connection.commit()
-                return True  # Request allowed
-
-
+                return True
 
 
 def get_session_history(session_id: str) -> BaseChatMessageHistory:
@@ -109,45 +87,27 @@ def create_llm_rag(user_id):
 
     embeddings = GigaChatEmbeddings(credentials=sber, verify_ssl_certs=False)
     vector_store = Chroma(
-                   persist_directory="/Users/gd/PycharmProjects/altirix_systems_chatbot/chromadb_chunk_size_1200",
+        persist_directory="/Users/gd/PycharmProjects/altirix_systems_chatbot/chromadb_chunk_size_1200",
         embedding_function=embeddings)
-        # persist_directory="/Users/gd/Desktop/altirix_bot_chromadbs/chroma_db_docs28885_ch_size500_ch_overlap50_gigachat_embs",
-        # embedding_function=embeddings)
-
-    # print(len(vector_store._collection.get(include=['embeddings'])['embeddings']))
-    # print(vector_store._collection_name)
     retriever_vanilla = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 8, })
-    retriever_mmr = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 8,})
-
+    retriever_mmr = vector_store.as_retriever(search_type="mmr", search_kwargs={"k": 8, })
 
     retriever_BM25 = BM25Retriever.from_texts(vector_store.get()['documents'])
 
     ensemble_retriever = EnsembleRetriever(
         retrievers=[retriever_vanilla, retriever_mmr, retriever_BM25], weights=[0.3, 0.3, 0.4]
     )
-
-    # embedding_retriever = MultiQueryRetriever.from_llm(
-    #     retriever=vector_store.as_retriever(
-    #
-    # ), llm=llm
-    # )
-
-    # compressor = FlashrankRerank()
-    # compression_retriever = ContextualCompressionRetriever(
-    #     base_compressor=compressor, base_retriever=embedding_retriever
-    # )
     history_aware_retriever = create_history_aware_retriever(
         llm, ensemble_retriever, contextualize_q_prompt
     )
 
-
-    rag_chain = create_rag_chain(user_id, llm, history_aware_retriever)
+    rag_chain = create_rag_chain(llm, history_aware_retriever)
 
     conversation_chain = create_conversation_chain(user_id, llm)
     return (vector_store, history_aware_retriever, llm, rag_chain, conversation_chain)
 
 
-def create_rag_chain(user_id, llm, embedding_retriever):
+def create_rag_chain(llm, embedding_retriever):
     document_chain = create_stuff_documents_chain(
         llm=llm,
         prompt=qa_prompt
@@ -172,12 +132,6 @@ def create_conversation_chain(user_id, llm):
     return (conversation)
 
 
-####################################################################################################
-#                                    Функции обработки команд                                      #
-####################################################################################################
-
-# `/start` - функция, обрабатывающая команду
-#############################################
 @bot.message_handler(commands=['start'])
 def start(message: types.Message):
     user_id = message.chat.id
@@ -194,8 +148,6 @@ def start(message: types.Message):
     bot.send_message(message.chat.id, 'Готов к работе')
 
 
-# `/help` - функция, обрабатывающая команду
-#############################################
 @bot.message_handler(commands=['help'])
 def help(message: types.Message):
     user_id = message.chat.id
@@ -204,12 +156,6 @@ def help(message: types.Message):
                      'Я - бот-помощник, отвечающий на вопросы по теме информационной безопасности.')
 
 
-####################################################################################################
-#                                  Функции обработки сообщений                                     #
-####################################################################################################
-
-# Функция, обрабатывающая неправильные форматы ввода
-####################################################
 @bot.message_handler(content_types=['audio',
                                     'video',
                                     'photo',
@@ -222,8 +168,6 @@ def not_text(message):
     bot.send_message(user_id, 'Я работаю только с текстовыми сообщениями!')
 
 
-# Функция, обрабатывающая текстовые сообщения
-#############################################
 @bot.message_handler(content_types=['text'])
 def handle_text_message(message):
     user_id = message.chat.id
@@ -237,33 +181,20 @@ def handle_text_message(message):
     vdb, embedding_retriever, llm, rag_chain, conversation = user_llm_rag[user_id]
     conversation.memory = user_conversations[user_id]
 
-    # print(user_conversations[user_id])
     q1 = message.text
 
-    for i in vdb.search(q1, search_type='similarity'):
-        print(i.page_content)
+    try:
+        if check_rate_limit(user_id, q1):
+            resp1 = rag_chain.invoke(
+                {'input': q1}, config={'configurable': {'session_id': user_id}}
+            )
 
-
-    if check_rate_limit(user_id, q1):
-    # with psycopg2.connect(**connection_params) as connection:
-    #     with connection.cursor() as cursor:
-    #         insert_query = """INSERT INTO user_queries (id_user, user_message, time_sent) VALUES (%s, %s, %s)"""
-    #
-    #         record_to_insert = (user_id, q1, datetime.datetime.now())
-    #
-    #         cursor.execute(insert_query, record_to_insert)
-    #         connection.commit()
-
-
-        resp1 = rag_chain.invoke(
-            {'input': q1}, config={'configurable': {'session_id': user_id}}
-        )
-
-        answer = resp1['answer']
-        bot.send_message(user_id, answer)
-    else:
-        bot.send_message(user_id, "Вы уже отправили боту 10 запросов за эти сутки. " )
-
+            answer = resp1['answer']
+            bot.send_message(user_id, answer)
+        else:
+            bot.send_message(user_id, "Вы уже отправили боту 10 запросов за эти сутки. ")
+    except ResponseError:
+        bot.send_message(user_id, "Ваш запрос слишком длинный. Пожалуйста, сократите его и попробуйте снова.")
 
     sleep(2)
 
